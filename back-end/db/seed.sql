26c60e2a7af0ac5c2319e74b5851a3ce4702b0be
Jan Matias <jancarlosmatias@gmail.com>
2022-12-01 19:54:00 -0500

    Fix small seed typo

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -8,7 +8,7 @@ INSERT INTO users (username, password, email, profileImg, address, zipcode, rati
 INSERT INTO items (userId, itemName, itemImg, category, description, isFound, request, giveaway, pinLocation, neighborhood, borough, zipcode, itemDate, status) VALUES 
 (1, 'itemname1', 'https://www.kroger.com/product/images/large/front/0079652034548', 'testcategory1', 'this is a fake item description.', false, false, false, 'testLongitud and testLatitude', 'Park Slope', 'Brooklyn', 00000, NOW(), 'Pending'),
 (2, 'itemname2', 'https://media.istockphoto.com/id/503237961/vector/cartoon-baseball-bat.jpg?s=612x612&w=0&k=20&c=1SCggGqrHPUeXYiGjkbiNS18EgFAXec7ZYAJ2p3krDA=', 'testcategory2', 'this is a fake item description.', false, false, false, 'testLongitud and testLatitude', 'Fordam Road', 'Bronx', 00000, NOW(), 'Completed'),
-(3, 'itemname3', 'https://m.media-amazon.com/images/I/51npNziCpTL._AC_UY1000_.jpg', 'testcategory3', 'this is a fake item description.', false, true, true, 'testLongitud and testLatitude', 'Sunnyside', 'Queens', 00000, NOW()), 'Donated',
+(3, 'itemname3', 'https://m.media-amazon.com/images/I/51npNziCpTL._AC_UY1000_.jpg', 'testcategory3', 'this is a fake item description.', false, true, true, 'testLongitud and testLatitude', 'Sunnyside', 'Queens', 00000, NOW(), 'Donated'),
 (1, 'itemname4', 'https://cdn.shopify.com/s/files/1/0514/6794/4099/products/stately_blue_brooklyn_space_modern_sofa_-_brooklyn_space_3.jpg?v=1645581175&width=1445', 'testcategory4', 'this is a fake item description.', false, false, true, 'testLongitud and testLatitude', 'Valentine Ave.', 'Manhattan', 00000, NOW(), 'Pending');
 
 INSERT INTO found_items (foundUserId, itemsId) VALUES 
