38cbb616d4bb43cddafb29a7148b8a2e3d60e6df
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-12-05 12:06:27 -0500

    merge

--- front-end/src/Components/Accounts/EditAccount/ViewUserSettings.js
+++ front-end/src/Components/Accounts/EditAccount/ViewUserSettings.js
@@ -1,13 +1,13 @@
 import "./Edit.scss";
 import "./ViewUserSettings.scss";
 import { useEffect, useState, React } from "react";
-<<<<<<< HEAD
 import { Button } from "react-bootstrap";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Edit from "./Edit";
+import FourOFour from "../../404/FourOFour";
 
-const ViewUserSettings = ({ users }) => {
+const ViewUserSettings = ({ user }) => {
   const API = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
 
@@ -15,23 +15,6 @@ const ViewUserSettings = ({ users }) => {
   const [userInfo, setUserInfo] = useState([]);
   const [editUser, setEditUser] = useState([]);
 
-=======
-import axios from "axios";
-import { Button } from "react-bootstrap";
-import { useParams, Link, useNavigate } from "react-router-dom";
-import Edit from "./Edit";
-import FourOFour from "../../404/FourOFour";
-
-const API = "http://localhost:4000";
-
-
-const ViewUserSettings = ({ user }) => {
-  const navigate = useNavigate();
-  const { userId } = useParams();
-  const [userInfo, setUserInfo] = useState([]);
-  const [editUser, setEditUser] = useState([]);
-
->>>>>>> main
   useEffect(() => {
     axios
       .get(`${API}/users/${userId}`)
@@ -55,12 +38,9 @@ const ViewUserSettings = ({ user }) => {
         </li>
       );
     } else {
-<<<<<<< HEAD
-      return null;
+      navigate("/404");
     }
   });
-
-  const currentUser = () => {};
 
   return (
     <div id="user-settings-div">
@@ -69,6 +49,7 @@ const ViewUserSettings = ({ user }) => {
         <h3>Personal Info</h3>
         <ul id="user-data-li">
           {userData}
+
           <Button
             variant="dark"
             onClick={() => {
@@ -91,30 +72,3 @@ const ViewUserSettings = ({ user }) => {
   );
 };
 export default ViewUserSettings;
-=======
-      navigate("/404");
-    }
-  });
-
-return (
-<div id='user-settings-div'>
-	<h1 className="edit-settings-heading"> Edit User settings</h1>
-	<section id='user-info-section'>
-	<h3>Personal Info</h3>
-	<ul id='user-data-li'>
-	{userData}
-
-		<Button variant='dark'onClick={() => {navigate(`/index`)}}>
-			Back
-		</Button>
-		<Button variant='success'onClick={() => {navigate(`/${userId}/edit`)}}>
-			Edit
-		</Button>
-	</ul>
-	</section>
-</div>
-	)
-};
-export default ViewUserSettings;
-
->>>>>>> main
