cd5560181eba7fe784ab4aad6b907c8b3047432e
Isaac Gonzalez <u1337x@gmail.com>
2022-12-06 10:34:59 -0500

    save maps work

--- front-end/src/Components/MapTest/MapTest.js
+++ front-end/src/Components/MapTest/MapTest.js
@@ -3,27 +3,20 @@ import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import "./Map.scss";
 
-const API = "AIzaSyDUykvCjIE7XHxPSsm7O4zL4b2ATw2e_c";
-
 export default function MapTest() {
-  console.log(API);
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: API,
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
   });
-
+  console.log(isLoaded.googleMapsApiKey);
   if (!isLoaded) return <div>Loading...</div>;
   return <Map />;
 }
 function Map() {
   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
-  console.log(center);
+
   return (
-    <GoogleMap
-      zoom={10}
-      center={{ lat: 44, lng: -80 }}
-      mapContainerClassName="map-container"
-    >
-      <Marker position={{ lat: 44, lng: -80 }} />
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
     </GoogleMap>
   );
 }
